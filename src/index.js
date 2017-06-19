775cf2be62ff8a1b03d014541b6c6cfc4e6f0a68
Lawynn Jana <jlawynn@gmail.com>
2017-06-19 15:05:26 -0400

    throttle search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail'
@@ -27,9 +28,12 @@ class App extends Component {
     });
   }
   render(){
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar onSearchChange={ term => this.videoSearch(term)}/>
+        <SearchBar onSearchChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) } videos={this.state.videos} />
