476719e90498c9e99929dac585426d95fcd9b1da
Lawynn Jana <jlawynn@gmail.com>
2017-06-19 14:28:50 -0400

    add playable video frame

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
+const API_KEY = 'AIzaSyA3MBOE0Ev_XsAzg_IARByd25P6mcSFbMY';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+
+      </div>
+    );
+  }
+
+}
+
+//display component's generated html into the DOM
+ReactDOM.render(<App/>, document.querySelector('.container'));
